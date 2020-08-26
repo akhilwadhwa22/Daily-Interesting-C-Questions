a4462ee26ff6df5e459ae228020c75e529fae020
Akhil Wadhwa <aw3509@nyu.edu>
2020-08-26 14:05:18 -0400

    Interesting use of "#"!!

---
+++ stringize.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define stringize(x) #x
+
+int main()
+{
+	printf(stringize(Hello World));
+	
+    return 0;
+}
+	
+    
+ // ````````````````````````````
+ // ` Follow: #letscwithakhilw `
+ // ` 						   `
+ // ````````````````````````````
