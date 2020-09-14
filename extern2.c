32529684b50fa35e7f45f12b520334af16bee664
Akhil Wadhwa <44124142+akhilwadhwa22@users.noreply.github.com>
2020-09-13 23:56:56 -0500

    Update extern2.c

--- extern2.c
+++ extern2.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-//extern int x;
+extern int x;
 
 
-// void foo(){
-// 	int x = 10;
-//     printf("%p\n", &x);
-// }
+void foo(){
+	int x = 10;
+    printf("%p\n", &x);
+}
 int main () 
 {
 	//case 1
-    // x= 10;
-    // printf("%d", x);
-	//foo();
+    x= 10;
+    printf("%d", x);
+    foo();
     //case 2
     int x = 10;
     printf("%p\n", &x);
 
     //case 3 : 
-	// extern int x =5;
- // 	x = 10;
- // 	printf("%d", x);
+    extern int x =5;
+    x = 10;
+    printf("%d", x);
     
     return 0;
 }
